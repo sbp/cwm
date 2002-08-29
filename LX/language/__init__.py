fb8611d9b8991423972b351be929b432523619b7
sandro <sandro>
2002-08-29 11:00:46 +0000

    initial version, mostly written or heavily rewritten over the past week (not thoroughly tested)

---
+++ LX/language/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+
+"""
+__version__ = "$Revision: 1.1 $"
+# $Id: __init__.py,v 1.1 2002-08-29 11:00:46 sandro Exp $
+
+
+__all__ = ["otter", "abstract"]
+
+# $Log: __init__.py,v $
+# Revision 1.1  2002-08-29 11:00:46  sandro
+# initial version, mostly written or heavily rewritten over the past
+# week (not thoroughly tested)
+#
