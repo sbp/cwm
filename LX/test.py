12872770035ec8723f70e85ed998409c44bfbc65
sandro <sandro>
2003-01-08 17:51:22 +0000

    improved output format

--- LX/test.py
+++ LX/test.py
@@ -2,19 +2,22 @@
 
 """
 
-__version__ = "$Revision: 1.1 $"
-# $Id: test.py,v 1.1 2003-01-08 17:48:27 sandro Exp $
+__version__ = "$Revision: 1.2 $"
+# $Id: test.py,v 1.2 2003-01-08 17:51:22 sandro Exp $
 
 import doctest
 import LX
 import LX.engine
 
 for x in [ "LX.expr", "LX.engine.otter" ]:
-    print "Module", x
+    print "Module", "%-25s" %x,
     __import__(x)
-    print eval("doctest.testmod(%s)" % x)
+    print "failed %3d of %3d tests." % eval("doctest.testmod(%s)" % x)
     
 # $Log: test.py,v $
-# Revision 1.1  2003-01-08 17:48:27  sandro
+# Revision 1.2  2003-01-08 17:51:22  sandro
+# improved output format
+#
+# Revision 1.1  2003/01/08 17:48:27  sandro
 # test harness for doctest across modules
 #
