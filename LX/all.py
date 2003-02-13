ceaa66ab4ca6aba611998417ece5565587fa99be
sandro <sandro>
2003-02-13 19:42:49 +0000

    first version

---
+++ LX/all.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+"""When you're feeling lazy and just want to import all of LX, you can
+do "from LX.all import *".
+
+"""
+
+__version__ = "$Revision: 1.1 $"
+# $Id: all.py,v 1.1 2003-02-13 19:42:49 sandro Exp $
+
+from LX import *
+from LX.uri import *
+from LX.kb import *
+from LX.expr import *
+from LX.logic import *
+from LX.describer import *
+from LX.namespace import *
+from LX.rdf import *
+from LX.defaultns import *
+
+# $Log: all.py,v $
+# Revision 1.1  2003-02-13 19:42:49  sandro
+# first version
+#
