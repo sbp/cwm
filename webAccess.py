90c280b203c95c8f0c69c90369571da6a4947370
connolly <connolly>
2004-01-29 23:32:11 +0000

    killed dead code

--- webAccess.py
+++ webAccess.py
@@ -1,34 +1,16 @@
 #!/usr/local/bin/python
 """
-$Id: webAccess.py,v 1.1 2004-01-29 23:28:59 timbl Exp $
+$Id: webAccess.py,v 1.2 2004-01-29 23:32:11 connolly Exp $
 
 Web access functionality building on urllib
 
 """
 
-
-
-import types
-import string
-import codecs # python 2-ism; for writing utf-8 in RDF/xml output
 import urllib
-
-from diag import verbosity, setVerbosity, progress
-
-from uripath import refTo, join
-
-import uripath
-import diag
 
 def urlopenForRDF(addr):
     """A version of urllib.urlopen() which asks for RDF by preference
     """
     z = urllib.FancyURLopener()
-#    z.addheaders.append(('Accept', 'application/rdf+xml, application/n3'))
-#    z.addheaders.append(('Accept', 'application/n3'))
     z.addheaders.append(('Accept', 'application/rdf+xml'))
-#    progress("HTTP Headers are now:", z.addheaders)
     return z.open(addr)
-
-
-#ends
