96c55110390f991fcdd58ad41af29fb8ee28a507
timbl <timbl>
2004-01-31 00:45:10 +0000

    Add COMPLETED:

--- pim/toIcal.py
+++ pim/toIcal.py
@@ -28,7 +28,7 @@ References:
   A quick look at iCalendar
   http://www.w3.org/2000/01/foo
 
-$Id: toIcal.py,v 2.6 2004-01-31 00:11:06 connolly Exp $
+$Id: toIcal.py,v 2.7 2004-01-31 00:45:10 timbl Exp $
 
 NOTE: see earlier work:
   http://www.w3.org/2002/01dc-nj/toICal.py
@@ -319,6 +319,7 @@ class CalWr:
         self.exportGeneral(E_PROP, sts, comp, ICAL.location, "LOCATION")
         self.exportGeneral(E_PROP, sts, comp, ICAL.priority, "PRIORITY")
         self.exportGeneral(E_PROP, sts, comp, ICAL.status, "STATUS")
+        self.timeProp(sts, "completed", comp)
 
 
         # notes on 4.8.4.6 Uniform Resource Locator
@@ -727,7 +728,10 @@ if __name__ == '__main__':
 
 
 # $Log: toIcal.py,v $
-# Revision 2.6  2004-01-31 00:11:06  connolly
+# Revision 2.7  2004-01-31 00:45:10  timbl
+# Add COMPLETED:
+#
+# Revision 2.6  2004/01/31 00:11:06  connolly
 # todo status support; no test; blech
 #
 # Revision 2.5  2004/01/29 21:28:13  timbl
