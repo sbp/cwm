9a7c799b89d76804a4121dc9439e524651728bb9
connolly <connolly>
2006-09-25 17:41:21 +0000

    oops; datatype URIs were goofy. and Exists

--- n3absyn.py
+++ n3absyn.py
@@ -11,7 +11,7 @@ but not other occurrences of {}.
 
 """
 
-__version__ = '$Id: n3absyn.py,v 1.4 2006-09-25 16:41:58 connolly Exp $'
+__version__ = '$Id: n3absyn.py,v 1.5 2006-09-25 17:41:21 connolly Exp $'
 
 import sys
 
@@ -223,7 +223,7 @@ def xml_form(f):
         # data
         elif head == 'data':
             assert(len(f) == 3)
-            yield '<Data type="%s">' % f[1]
+            yield '<Data type="%s">' % f[1][0]
             yield escape(f[2])
             yield "</Data>"
             rest = f[1:]
@@ -259,13 +259,14 @@ def xml_form(f):
             
         # quantifiers
         elif head == 'exists':
-            yield "<Exists>\n" % tagname
+            yield "<Exists>\n"
             for v in f[1]:
                 yield "<Var>%s</Var>" % v
             yield '\n'
             for part in f[2:]:
                 for s in xml_form(part):
                     yield s
+            yield "</Exists>\n"
 
         elif head == 'forall':
             #@@hmm... treat other vars as implicitly universally quanitified?
