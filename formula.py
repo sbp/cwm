b76fc9fb04e81d1d45a4c00a62c7c7e2499811d3
syosi <syosi>
2007-03-30 19:05:40 +0000

    fix for http://lists.w3.org/Archives/Public/public-cwm-bugs/2007Feb/0000.html

--- formula.py
+++ formula.py
@@ -2,7 +2,7 @@ from __future__ import generators
 #! /usr/bin/python
 """
 
-$Id: formula.py,v 1.54 2007-02-13 21:35:32 syosi Exp $
+$Id: formula.py,v 1.55 2007-03-30 19:05:40 syosi Exp $
 
 Formula
 See:  http://www.w3.org/DesignIssues/Notation3
@@ -21,7 +21,7 @@ and the redfoot/rdflib interface, a python RDF API:
 
 """
 
-__version__ = '$Id: formula.py,v 1.54 2007-02-13 21:35:32 syosi Exp $'[1:-1]
+__version__ = '$Id: formula.py,v 1.55 2007-03-30 19:05:40 syosi Exp $'[1:-1]
 
 import types
 import StringIO
@@ -383,6 +383,8 @@ For future reference, use newUniversal
             subj=s[SUBJ].substitution(
                                  bindings2, why=subWhy, cannon=cannon).substitution(
                                     bindings3, why=subWhy, cannon=cannon)
+            ### Make sure we don't keep making copies of the same formula from running
+            ##  the same rule again and again
             if isTopLevel(self) and isinstance(subj, Formula) and not subj.reallyCanonical:
                 subj = subj.reopen()
                 subj = subj.canonicalize(cannon=True)
@@ -396,7 +398,11 @@ For future reference, use newUniversal
 	    obj=s[OBJ].substitution(
                                  bindings2, why=subWhy, cannon=cannon).substitution(
                                     bindings3, why=subWhy, cannon=cannon)
+            if isTopLevel(self) and isinstance(obj, Formula) and not obj.reallyCanonical:
+                obj = obj.reopen()
+                obj = obj.canonicalize(cannon=True)
 	    if obj is not s[OBJ]:
+                ### Question to self: What is this doing?
                 bindings2[s[OBJ]] = obj
                 
 	    total += self.add(subj=subj,
